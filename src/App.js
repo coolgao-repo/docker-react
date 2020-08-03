ca63bcfe595f4490c07f72959cc4c07e5c3904b6
Sky <coolgao.gofly@gmail.com>
2020-08-03 16:19:50 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Docker-Git-Travis Learn!
         </a>
       </header>
     </div>
